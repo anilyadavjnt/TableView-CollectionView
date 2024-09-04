40e32b4edead376909933e6f5ec7ee243b0ad855
Anil <anilyadavjnt@gmail.com>
2024-09-04 17:20:59 +0530

    Initial Commit

---
+++ Golu/Controller/ExploreCableCell/ExploreTableViewCell.swift
@@ -0,0 +1,42 @@
+//
+//  ExploreTableViewCell.swift
+//  Golu
+//
+//  Created by Anil Yadav on 04/09/24.
+//
+
+import UIKit
+
+class ExploreTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
+
+extension ExploreTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 7
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 150, height: 190)
+    }
+}
---
+++ Golu/ExploreViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ExploreViewController.swift
+//  Golu
+//
+//  Created by Anil Yadav on 04/09/24.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
+
+extension ExploreViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ExploreTableViewCell", for: indexPath) as? ExploreTableViewCell else {fatalError("Unable to creat explore table view cell")}
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 300
+    }
+    
+}
